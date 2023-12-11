26b4271cdc49f1b1b9974df197e1f4b82984bbb8
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-11 13:24:23 +0900

    add TweetRequest

--- server/app/Http/Controllers/Api/TweetController.php
+++ server/app/Http/Controllers/Api/TweetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\TweetRequest;
 use App\Models\Tweet;
 use Illuminate\Http\Request;
 
---
+++ server/app/Http/Requests/Api/TweetRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class TweetRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        // return false;
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'user_id' => ['required', 'number'],
+            'message' => ['required'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json([
+            'status' => 400,
+            'errors' => $validator->errors(),
+        ],400);
+        throw new HttpResponseException($res);
+    }
+}
