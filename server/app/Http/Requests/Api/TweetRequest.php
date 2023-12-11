f71c4234cb5840112179805b185d4443f7873db3
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-11 13:34:00 +0900

    update

--- server/app/Http/Requests/Api/TweetRequest.php
+++ server/app/Http/Requests/Api/TweetRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests\Api;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class TweetRequest extends FormRequest
 {
@@ -17,7 +18,11 @@ class TweetRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user_id' => ['required', 'integer'],
+            'user_id' => [
+                'required',
+                'integer',
+                Rule::exists('users', 'id'),
+            ],
             'message' => ['required'],
         ];
     }
