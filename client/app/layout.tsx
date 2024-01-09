5cc2575c8840a17cbbca4348278580c33f93a038
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-09 11:50:20 +0900

    add Navbar

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Navbar from '@/app/components/Navbar'
 
 export const metadata: Metadata = {
   title: 'Next Tweet',
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className='flex min-h-screen flex-col p-2'>
+        <Navbar />
         {children}
       </body>
     </html>
