213ba4b467896815bb5381e881dbcfc33ef03cab
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-15 11:43:17 +0900

    update

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\TweetController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RegistUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,9 @@ Route::middleware('auth:sanctum')->group(function () {
 
 // AuthController
 Route::post('/auth', [AuthController::class, 'auth']);
+
+// RegistController
+Route::post('/regist/store', [RegistUserController::class, 'store']);
 
 // /api/user
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
