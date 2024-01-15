741234528150a5584a44d5d8ebd9ac5f7d9e0ce7
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-15 11:04:05 +0900

    update

--- server/routes/api.php
+++ server/routes/api.php
@@ -9,7 +9,10 @@ use Illuminate\Support\Facades\Route;
 //ルーティングのURL：「/api/xxxx」
 // TweetController
 Route::get('tweet/get', [TweetController::class, 'get']);
-Route::post('tweet/add', [TweetController::class, 'add']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('tweet/add', [TweetController::class, 'add']);
+});
 
 // AuthController
 Route::post('/auth', [AuthController::class, 'auth']);
