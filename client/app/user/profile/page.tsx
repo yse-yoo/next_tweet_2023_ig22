307e875f1dc7f779f9b8d8b627925d4886f803cb
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-16 09:28:49 +0900

    update

--- client/app/user/profile/page.tsx
+++ client/app/user/profile/page.tsx
@@ -5,8 +5,13 @@ import imageMe from '@/public/images/me.png';
 const ProfilePage = () => {
     return (
         <div>
-            <h1 className="text-3xl text-center font-bold">Profile</h1>
-            <Image src={imageMe} alt="" />
+            <h1 className="text-3xl text-center font-bold p-3">Profile</h1>
+            <div className="flex justify-center p-3">
+                <Image src={imageMe} className="h-36 w-36 rounded-full" alt="" />
+            </div>
+            <div className="flex justify-center p-3">
+                Dummy User
+            </div>
         </div>
     );
 }
