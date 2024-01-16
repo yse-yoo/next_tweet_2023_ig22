6a68e88864fa7ad423d57f7a0d07597afec26c11
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-16 09:41:09 +0900

    update

--- client/app/auth/regist/page.tsx
+++ client/app/auth/regist/page.tsx
@@ -1,7 +1,12 @@
+import { FaUser } from "react-icons/fa";
+
 const RegistPage = () => {
     return (
         <div>
-            Enter
+            <h1 className="p-3 flex justify-center text-3xl font-bold">
+                <FaUser className="mt-1 me-3" />
+                Sign up
+            </h1>
         </div>
     );
 }
