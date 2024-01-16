8a0d681f84635d3005a2f3d24a9e40d75017b414
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-16 10:22:49 +0900

    update

--- client/app/auth/regist/page.tsx
+++ client/app/auth/regist/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { FaUser } from "react-icons/fa";
+import Link from "next/link";
 import Input from "@/app/components/Input";
 
 const RegistPage = () => {
     return (
         <div className="mx-auto w-1/3">
-            <h1 className="mt-2 p-1 flex justify-center text-2xl font-bold">
+            <h1 className="my-2 p-1 flex justify-center text-2xl font-bold">
                 <FaUser className="mt-2 me-2" />
                 Sign up
             </h1>
@@ -15,6 +16,29 @@ const RegistPage = () => {
                 <Input type="text" placeholder="Your Name" />
                 <Input type="text" placeholder="Email" />
                 <Input type="password" placeholder="******" />
+            </div>
+
+            <div>
+                <button className="
+                            w-full
+                          bg-blue-500 hover:bg-blue-700
+                          text-white font-bold 
+                          py-3 px-4 mb-2
+                          rounded
+                         ">
+                    Sign up
+                </button>
+                <Link
+                    href="/auth/login"
+                    className="
+                            flex justify-center
+                          bg-gray-200 hover:bg-gray-300
+                          text-gray-500 font-bold 
+                          py-3 px-4 
+                          rounded
+                         ">
+                    Sing in
+                </Link>
             </div>
         </div>
     );
