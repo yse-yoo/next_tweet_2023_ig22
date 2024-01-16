b152e8c1de832448c74ae791bc2fb2c891f1d372
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-16 10:11:13 +0900

    update

--- client/app/components/Input.tsx
+++ client/app/components/Input.tsx
@@ -1,7 +1,14 @@
 
-const Input = ({type, placeholder}) => {
+interface InputProps {
+    type: string;
+    value?: string;
+    placeholder?: string;
+}
+
+const Input = ({type, value, placeholder} : InputProps) => {
     return (
         <input type={type}
+            value={value}
             placeholder={placeholder}
             className="my-2 border-2 
                 border-gray-200
