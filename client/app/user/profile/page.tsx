19ae1e8a5801b36ced27444ba5ebaae9f1006cd5
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-16 09:21:58 +0900

    show image

--- client/app/user/profile/page.tsx
+++ client/app/user/profile/page.tsx
@@ -1,7 +1,12 @@
+
+import Image from 'next/image';
+import imageMe from '@/public/images/me.png';
+
 const ProfilePage = () => {
     return (
         <div>
             <h1 className="text-3xl text-center font-bold">Profile</h1>
+            <Image src={imageMe} alt="" />
         </div>
     );
 }
