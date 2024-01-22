06f2f3c7a7257f295e5791a2b12643c9bb5657cd
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-22 10:32:02 +0900

    update

--- client/app/services/TweetService.ts
+++ client/app/services/TweetService.ts
@@ -3,8 +3,19 @@ import { User } from "@/app/models/User";
 const LARAVEL_API_URL = process.env.NEXT_PUBLIC_LARAVEL_API_URL;
 
 // Tweet取得
-export const getTweets = (accessToken:string) => {
+export const getTweets = async (accessToken:string) => {
+    const url = LARAVEL_API_URL + "tweet/get"
+    const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+            }
+        });
 
+    if (response.ok) {
+        return await response.json();
+    }
 }
 
 // Tweet投稿
