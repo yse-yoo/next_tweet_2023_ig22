746becb6c9d2f83f59f62eae19390129e1afb2a5
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-22 13:50:36 +0900

    update

--- client/app/components/tweet/TweetForm.tsx
+++ client/app/components/tweet/TweetForm.tsx
@@ -4,17 +4,18 @@ const TweetForm = () => {
     const [message, setMessage] = useState<string>("")
 
     const messageHandler = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
-        console.log(event.target.value)
+        setMessage(event.target.value)
     }
 
     return (
         <div>
             <textarea 
+                value={message}
                 onChange={messageHandler}
                 className="resize-none 
                     w-full h-24 border rounded-md p-2"
                 placeholder="今なにしてる？"></textarea>
-
+            <div className="p-3">{message.length} characters.</div>
             <button 
                 className="bg-blue-500 hover:bg-blue-700 
                     text-white font-bold 
