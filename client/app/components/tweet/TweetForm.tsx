ac8317a8ea3d3ae253ce9b9cb2fb3e46cea2287f
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-22 13:37:59 +0900

    add TweetForm

---
+++ client/app/components/tweet/TweetForm.tsx
@@ -0,0 +1,11 @@
+
+const TweetForm = () => {
+    return (
+        <div>
+            <textarea className="resize-none w-full h-24 border rounded-md p-2" placeholder="今なにしてる？"></textarea>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Send</button>
+        </div>
+    );
+}
+
+export default TweetForm;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,6 +5,7 @@ import { getTweets } from "./services/TweetService"
 import { User, testUser } from "./models/User"
 import { Tweet } from "./models/Tweet"
 import TweetList from "./components/tweet/TweetList"
+import TweetForm from "./components/tweet/TweetForm"
 
 export default function Home() {
   const [user, setUser] = useState<User>(testUser)
@@ -25,8 +26,7 @@ export default function Home() {
 
   return (
     <div>
-      <textarea className="resize-none w-full h-24 border rounded-md p-2" placeholder="今なにしてる？"></textarea>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Send</button>
+      <TweetForm />
 
       <TweetList initialTweets={tweets} />
     </div>
