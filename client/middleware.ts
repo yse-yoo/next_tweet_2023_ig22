d25e2d741e8450503aca7ea2e63f2fb553dea080
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-29 10:59:10 +0900

    add middleware

---
+++ client/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const authURL = process.env.BASE_URL + "auth/login";
+
+export async function middleware(req: NextRequest) {
+    const token = req.cookies.get('access_token');
+    console.log("Middleware:", token)
+    if (!token) return NextResponse.redirect(authURL);
+
+    return NextResponse.next();
+}
+
+// Middlewareのスコープ
+export const config = {
+    matcher: ["/", "/user/:path*", "/user/:slug*"]
+};
