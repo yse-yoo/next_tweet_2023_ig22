a7c89f4997bc9ebbab00c41f50bd4f6d2ae42824
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-29 13:59:57 +0900

    update

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 
             <div className="text-sm md:flex-grow">
                 {
-                    (user == undefined || user?.id > 0) ?
+                    (user?.id > 0) ?
                         <>
                             <NavbarLink href="/user/profile" label="Profile" />
                             <NavbarLink href="#" label="Sign out" onClick={signOut} />
