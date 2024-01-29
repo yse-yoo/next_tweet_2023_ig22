99d3fc324ee885cb8300ca42c9b89ac584f4b9cb
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-29 10:01:58 +0900

    add UserProvider

---
+++ client/app/providers/UserProvider.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+import UserContext from "../context/UserContext";
+import { initialUser } from "../models/User";
+import Cookies from "js-cookie";
+import { getUser } from "../services/UserService";
+
+export default function UserProvider({
+    children,
+}: {
+    children: React.ReactNode,
+}): React.ReactNode {
+    const [user, setUser] = useState(initialUser)
+
+    //トークンからユーザ取得
+    useEffect(() => {
+        (async () => {
+            const token = Cookies.get('access_token') || "";
+            const user = await getUser(token);
+            console.log("AuthProvider:", user)
+            setUser(user);
+        })();
+    }, [])
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
