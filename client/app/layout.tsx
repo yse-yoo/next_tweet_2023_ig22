89a16f425c713fb3670f04f55804dc9c6320c1fa
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-30 11:08:32 +0900

    update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,28 +1,32 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Navbar from '@/app/components/Navbar'
-// import UserProvider from '@/app/providers/UserProvider'
-import { NextAuthProvider } from '@/app/providers/NextAuthProvider'
+import Navbar from '@/app/components/Navbar';
+import UserProvider from "@/app/providers/UserProvider"
+import { NextAuthProvider } from './providers/NextAuthProvider';
+
+// import AuthContext from './context/UserContext';
 
 export const metadata: Metadata = {
   title: 'Next Tweet',
-  description: 'This is tweet app',
+  description: 'This is tweet app sample.',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
 }) {
   return (
     <html lang="ja">
-      <body className='flex min-h-screen flex-col p-2'>
+      <body>
+        {/* <UserProvider> */}
         <NextAuthProvider>
           <Navbar />
-          <main className="m-10">
+          <main className="flex min-h-screen flex-col p-5">
             {children}
           </main>
         </NextAuthProvider>
+        {/* </UserProvider> */}
       </body>
     </html>
   )
