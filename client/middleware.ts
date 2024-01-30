07e45a78a0e855831aac092e6527c1c63ddd0705
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-30 09:16:17 +0900

    add route

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,3 +1,7 @@
+// export { default } from "next-auth/middleware"
+
+// export const config = { matcher: ["/"] }
+
 import { NextRequest, NextResponse } from 'next/server';
 
 const authURL = process.env.BASE_URL + "auth/login";
