6de666007aa49a31e5816dee1637bc6774ae1571
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-30 09:38:09 +0900

    update middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,19 +1,21 @@
-// export { default } from "next-auth/middleware"
-
-// export const config = { matcher: ["/"] }
-
-import { NextRequest, NextResponse } from 'next/server';
-
-const authURL = process.env.BASE_URL + "auth/login";
-
-export async function middleware(req: NextRequest) {
-    console.log('--- middleware ---')
-    const token = req.cookies.get('access_token');
-    console.log(token)
-    if (!token) return NextResponse.redirect(authURL);
-    return NextResponse.next();
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: ["/", "/user/:path*", "/user/:slug*"]
+matcher: ["/", "/user/:path*", "/user/:slug*"]
 };
+
+// import { NextRequest, NextResponse } from 'next/server';
+
+// const authURL = process.env.BASE_URL + "auth/login";
+
+// export async function middleware(req: NextRequest) {
+//     console.log('--- middleware ---')
+//     const token = req.cookies.get('access_token');
+//     console.log(token)
+//     if (!token) return NextResponse.redirect(authURL);
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ["/", "/user/:path*", "/user/:slug*"]
+// };
