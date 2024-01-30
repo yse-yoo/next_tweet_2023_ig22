71b4ed6b785fce5034b47c6a55b098b33197837e
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-30 09:39:32 +0900

    update

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,7 +1,7 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-matcher: ["/", "/user/:path*", "/user/:slug*"]
+    matcher: ["/", "/user/:path*", "/user/:slug*"]
 };
 
 // import { NextRequest, NextResponse } from 'next/server';
